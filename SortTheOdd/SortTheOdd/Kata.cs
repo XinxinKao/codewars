7807fcfecdcf15fea7b1ddcc1c6b06a663ea39e7
xinyi <xinyi.kao@titansoft.com.sg>
2018-04-14 23:09:33 +0800

    finish Kata

--- SortTheOdd/SortTheOdd/Kata.cs
+++ SortTheOdd/SortTheOdd/Kata.cs
@@ -1,10 +1,39 @@
-﻿namespace SortTheOdd
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SortTheOdd
 {
 	public class Kata
 	{
 		public static int[] SortArray(int[] array)
 		{
+			if(array.Length == 0)
+				return array;
+
+			var oddNumberIndex = new List<int>{};
+			var oddNumbers = new List<int>{};
+			
+			for (var i = 0; i < array.Length; i++)
+			{
+				if (IsOdd(array[i]))
+				{
+					oddNumberIndex.Add(i);
+					oddNumbers.Add(array[i]);
+				}
+			}
+
+			var orderOddNumbers = oddNumbers.OrderBy(i => i).ToList();
+			for (var j = 0; j < oddNumberIndex.Count; j++)
+			{
+				array[oddNumberIndex[j]] = orderOddNumbers[j];
+			}
+
 			return array;
+		}
+
+		private static bool IsOdd(int number)
+		{
+			return number % 2 != 0;
 		}
 	}
 }
--- SortTheOdd/SortTheOddTest/UnitTest1.cs
+++ SortTheOdd/SortTheOddTest/UnitTest1.cs
@@ -1,14 +1,18 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using NUnit.Framework;
+using SortTheOdd;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 
 namespace SortTheOddTest
 {
-	[TestClass]
-	public class UnitTest1
+	[TestFixture]
+	public class KataTests
 	{
-		[TestMethod]
-		public void TestMethod1()
+		[Test]
+		public void BasicTests()
 		{
+			CollectionAssert.AreEqual(new int[] { 1, 3, 2, 8, 5, 4 }, Kata.SortArray(new int[] { 5, 3, 2, 8, 1, 4 }));
+			CollectionAssert.AreEqual(new int[] { 1, 3, 5, 8, 0 }, Kata.SortArray(new int[] { 5, 3, 1, 8, 0 }));
+			CollectionAssert.AreEqual(new int[] { }, Kata.SortArray(new int[] { }));
 		}
 	}
 }

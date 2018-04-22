ece340a123974268069160282b53677fdbf66fd0
xinyi <xinyi.kao@titansoft.com.sg>
2018-04-22 22:17:54 +0800

    create new project : CountingChangeCombinations

---
+++ CountingChangeCombinations/CountingChangeCombinationsTest/CodeWarsTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CountingChangeCombinationsTest
+{
+	[TestClass]
+	public class CodeWarsTest
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
---
+++ CountingChangeCombinations/CountingChangeCombinationsTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CountingChangeCombinationsTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CountingChangeCombinationsTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1ed83cab-3d46-47f4-9e22-f7bd5a8c1675")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

c73f8520e97cd23d5c41c9deac7046688549f879
xinyi <xinyi.kao@titansoft.com.sg>
2018-05-13 15:04:02 +0800

    add test case

---
+++ MultiplesOf3Or5/MultiplesOf3Or5Test/Test.cs
@@ -0,0 +1,16 @@
+﻿using MultiplesOf3Or5;
+using NUnit.Framework;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace MultiplesOf3Or5Test
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void Test()
+		{
+			Assert.AreEqual(23, Kata.Solution(10));
+		}
+	}
+}

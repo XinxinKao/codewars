5eaf07c713b619e457b2c18bb7b4648a2c811e27
xinyi <xinyi.kao@titansoft.com.sg>
2018-05-13 14:59:13 +0800

    add new Kata Multiples of 3 or 5

---
+++ MultiplesOf3Or5/MultiplesOf3Or5/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplesOf3Or5
+{
+	public class Kata
+	{
+		public static int Solution(int value)
+		{
+			return 0;
+		}
+	}
+}

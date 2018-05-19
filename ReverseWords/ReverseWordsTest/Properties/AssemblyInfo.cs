a423208bb896380238796f429e13e35e9693fdc3
xinyi <xinyi.kao@titansoft.com.sg>
2018-05-19 13:43:43 +0800

    add test project

---
+++ ReverseWords/ReverseWordsTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ReverseWordsTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReverseWordsTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5bc4d867-e23d-48ac-81cd-6949e0a8c20c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ReverseWords/ReverseWordsTest/Tests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using ReverseWords;
+
+namespace ReverseWordsTest
+{
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void Example()
+		{
+			Assert.AreEqual("sihT si na !elpmaxe", Kata.ReverseWords("This is an example!"));
+		}
+	}
+}

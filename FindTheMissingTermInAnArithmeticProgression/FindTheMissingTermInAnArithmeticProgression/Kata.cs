20903680a1b3a8d948e62c900b6542b5d3c7f850
xinyi <xinyi.kao@titansoft.com.sg>
2018-05-27 21:21:08 +0800

    add new kata Highest Rank Number in an Array

---
+++ FindTheMissingTermInAnArithmeticProgression/FindTheMissingTermInAnArithmeticProgression/Kata.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindTheMissingTermInAnArithmeticProgression
+{
+    public class Kata
+    {
+	    public static int FindMissing(List<int> list)
+	    {
+		    var x = 0;
+		    var y = 0;
+		    for (int i = 0; i < list.Count - 1; i++)
+		    {
+			    x = list[i + 1] - list[i];
+				
+		    }
+
+		    return list[0];
+	    }
+	}
+}

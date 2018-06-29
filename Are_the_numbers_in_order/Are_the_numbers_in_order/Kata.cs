9b9519797b1248cf9d6a4d5f7125a1be27a64c2d
xinyi <xinyi.kao@titansoft.com.sg>
2018-06-29 17:41:10 +0800

    add new kata

---
+++ Are_the_numbers_in_order/Are_the_numbers_in_order/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Are_the_numbers_in_order
+{
+    public class Kata
+    {
+	    public static bool IsAscOrder(int[] arr)
+	    {
+		    return true;
+	    }
+	}
+}
---
+++ Are_the_numbers_in_order/Are_the_numbers_in_order_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Are_the_numbers_in_order_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Are_the_numbers_in_order_Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e3ad9648-f5aa-4ba6-b1a1-f12feff7fe9e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

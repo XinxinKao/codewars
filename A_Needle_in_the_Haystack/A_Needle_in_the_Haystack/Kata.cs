22b0f8eae32fc29653b63adeeeb056cdfff23608
xinyi <xinyi.kao@titansoft.com.sg>
2018-06-29 15:21:26 +0800

    add new Kata

---
+++ A_Needle_in_the_Haystack/A_Needle_in_the_Haystack/Kata.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_Needle_in_the_Haystack
+{
+    public class Kata
+    {
+	    public static string FindNeedle(object[] haystack)
+	    {
+		    //Code goes here!
+			return string.Empty;
+	    }
+	}
+}
---
+++ A_Needle_in_the_Haystack/A_Needle_in_the_Haystack_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("A_Needle_in_the_Haystack_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("A_Needle_in_the_Haystack_Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("974fbdad-979d-416b-95f3-5267d831d5a6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

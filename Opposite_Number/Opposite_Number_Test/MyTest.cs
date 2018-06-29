c0569c776ac86a9af49cc594e770eaf916f997ea
xinyi <xinyi.kao@titansoft.com.sg>
2018-06-29 17:30:44 +0800

    add new kata

---
+++ Opposite_Number/Opposite_Number_Test/MyTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using Opposite_Number;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace Opposite_Number_Test
+{
+	[TestFixture]
+	public class MyTest
+	{
+		[Test]
+		public void Test_1()
+		{
+			Assert.AreEqual(-1, Kata.Opposite(1));
+		}
+	}
+}
---
+++ Opposite_Number/Opposite_Number_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Opposite_Number_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Opposite_Number_Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e8723b24-8366-464d-9009-b9a6cde6a834")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

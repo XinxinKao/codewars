bbf1a98579f34ac640ceb4e409f16aabbfad1603
xinyi <xinyi.kao@titansoft.com.sg>
2018-06-29 17:20:45 +0800

    refactor kata

--- Simple_Multiplication/Simple_Multiplication/Multiplier.cs
+++ Simple_Multiplication/Simple_Multiplication/Multiplier.cs
@@ -8,21 +8,14 @@ namespace Simple_Multiplication
 {
     public class Multiplier
     {
-	    public static int Multiply(int x)
+		public static int Multiply(int x)
 	    {
-		    if (IsOddNumber(x))
-		    {
-			    return x * 8;
-		    }
-		    else
-		    {
-			    return x * 9;
-		    }
+		    return x * (IsOddNumber(x) ? 8 : 9);
 	    }
 
 	    private static bool IsOddNumber(int x)
 	    {
 		    return x % 2 == 0;
 	    }
-    }
+	}
 }

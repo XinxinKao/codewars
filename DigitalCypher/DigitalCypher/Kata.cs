8e846e4798526f51f62f7507f7cafea9d65ab3a8
xinyi <xinyi.kao@titansoft.com.sg>
2018-08-23 17:39:55 +0800

    finish Kata and add test case

--- DigitalCypher/DigitalCypher/Kata.cs
+++ DigitalCypher/DigitalCypher/Kata.cs
@@ -1,16 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DigitalCypher
+﻿namespace DigitalCypher
 {
     public class Kata
     {
-	    public static int[] Encode(string str, int n)
+	    public static int[] Encode(string input, int number)
 	    {
-		    return str;
+		    var result = new int[input.Length];
+		    var inputChars = input.ToCharArray();
+		    var numberChars = number.ToString().ToCharArray();
+		    for (var i = 0; i < input.Length; i++)
+		    {
+			    var numberIndex = i % numberChars.Length;
+			    result[i] = TransferCharToNumberByAscii(inputChars[i], '`') +TransferCharToNumberByAscii(numberChars[numberIndex], '0');
+		    }
+		    return result;
+	    }
+
+	    private static int TransferCharToNumberByAscii(char character1, char character2)
+	    {
+		    return character1 - character2;
 	    }
     }
 }
--- DigitalCypher/DigitalCypherTest/CypherTest.cs
+++ DigitalCypher/DigitalCypherTest/CypherTest.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using DigitalCypher;
+﻿using DigitalCypher;
 using NUnit.Framework;
-using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 
 namespace DigitalCypherTest
 {
@@ -11,9 +9,16 @@ namespace DigitalCypherTest
 		[Test]
 		public void BasicTest()
 		{
-			Assert.AreEqual(new int[] {20, 12, 18, 30, 21}, Kata.Encode("scout", 1939));
-			Assert.AreEqual(new int[] {14, 10, 22, 29, 6, 27, 19, 18, 6, 12, 8}, Kata.Encode("masterpiece", 1939));
-       
+			CollectionAssert.AreEqual(new int[] { 20, 12, 18, 30, 21 }, Kata.Encode("scout", 1939));
+			CollectionAssert.AreEqual(new int[] { 14, 10, 22, 29, 6, 27, 19, 18, 6, 12, 8 }, Kata.Encode("masterpiece", 1939));
+		}
+
+		[Test]
+		public void RandomTest()
+		{
+			CollectionAssert.AreEqual(new int[] { 14, 24 }, Kata.Encode("mo", 1937146320));
+			CollectionAssert.AreEqual(new int[] { 7, 19, 6, 22, 27, 27, 4}, Kata.Encode("fncvrsc", 1530981613));
+			CollectionAssert.AreEqual(new int[] { 25, 22, 17, 18, 24, 22, 27, 13, 32, 15, 5, 23, 31, 28, 26, 5, 30, 21, 28, 23, 22, 12, 9, 31, 10, 17, 24, 3, 13, 16, 17, 21, 8, 22, 16 }, Kata.Encode("wthkuoyiymcnxyscznzumefxhmqakgjratl", 229737247));
 		}
 	}
 }

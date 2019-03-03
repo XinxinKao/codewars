ac05636b37851bbdd4147f5814a7d565ab71a888
xinyi <xinyi.kao@titansoft.com.sg>
2019-03-03 23:02:30 +0800

    Finish kata

--- MaximumLengthDifference/MaximumLengthDifference/MaxDiffLength.cs
+++ MaximumLengthDifference/MaximumLengthDifference/MaxDiffLength.cs
@@ -10,7 +10,14 @@ namespace MaximumLengthDifference
     {
 	    public static int Mxdiflg(string[] a1, string[] a2)
 	    {
-		    return 0;
+		    if (a1.Length == 0 || a2.Length == 0)
+			    return -1;
+
+		    var firstResult = Math.Abs(a1.Max(x => x.Length) - a2.Min(x => x.Length));
+
+		    var secondResult = Math.Abs(a2.Max(x => x.Length) - a1.Min(x => x.Length));
+			
+		    return Math.Max(firstResult, secondResult);
 	    }
     }
 }
